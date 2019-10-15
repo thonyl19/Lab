8afadc92de7deb94a0b93ced52178ddbfea9fc51
thony <a0982830615@gmail.com>
2019-10-15 11:43:12 +0800

    Add>基礎路徑

--- vuepress_1x/docs/README.md
+++ vuepress_1x/docs/README.md
@@ -1,1 +1,3 @@
 # Hello VuePress!
+![An image](../docs/static/img/test.png)
+<img :src="$withBase('/assets/img/test.png')" alt="foo">
--- vuepress_1x/docs/config.js
+++ vuepress_1x/docs/config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    configureWebpack: {
+      resolve: {
+        alias: {
+          'img': '../docs/static/img/'
+        }
+      }
+    },
     title: 'Hello VuePress',
     description: 'Just playing around'
   }
