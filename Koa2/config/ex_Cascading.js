61299956ad2a4c839358d95fb14e40ce3d3c189a
thony <a0982830615@gmail.com>
2020-03-29 21:49:14 +0800

    Add

---
+++ Koa2/config/ex_Cascading.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`[事件1]${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+  console.log(`[事件2]X-Response-Time`);
+  
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
--- Koa2/readm.md
+++ Koa2/readm.md
@@ -1,5 +1,27 @@
-安装 `koa`
+### Note
 
+    (官網)[https://koajs.com/]
+
+
+
+### 起手式
+
+安装 `koa`
 ```
 npm i koa -D
+```
+
+### 基本用例 
+    (base_server.js)[./config/base_server.js]
+
+### Cascading
+    (base_server.js)[./config/ex_Cascading.js]
+
+### app.listen（...）
+
+### 錯誤處理
+```
+app.on('error', err => {
+  log.error('server error', err)
+});
 ```
